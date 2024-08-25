24c730f1703542ee96209a65111e02e29453e16c
Anastasia <nastya.khmelnikova@gmail.com>
2024-08-25 16:28:33 +0300

    second commit

---
+++ src/test/java/ru/netology/service/CashbackHackerTest.java
@@ -0,0 +1,40 @@
+package ru.netology.service;
+
+import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CashbackHackerTest {
+    CashbackHacker service = new CashbackHacker();
+
+    @Test
+    public void shouldCalculateIfAmountCorrect() {
+        int amount = 5700;
+        int actual = service.remain(amount);
+        int expected = 300;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldCalculateIfAmountZero() {
+        int amount = 0;
+        int actual = service.remain(amount);
+        int expected = 1000;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldCalculateIfAmount1000() {
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldCalculateIfAmountIncorrect() {
+        int amount = -50000;
+        int actual = service.remain(amount);
+        int expected = 0;
+        assertEquals(expected, actual);
+    }
+}
